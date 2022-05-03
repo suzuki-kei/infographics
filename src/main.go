864b73926c8383b3cec1266d7260c91c53d22056
SUZUKI Kei <todokimasen@gmail.com>
2022-05-03 15:50:04 +0900

    コマンドラインから変換する値を指定できるように実装した.

--- src/main.go
+++ src/main.go
@@ -2,21 +2,15 @@ package main
 
 import (
     "fmt"
+    "os"
+    "strings"
 )
 
 func main() {
-    stringValues := []string {
-        "0",
-        "12345",
-        "70000",
-        "100000",
-        "1000000",
-        "1800000",
-        "10000000000",
-        "17500000000",
-    }
+    options := parseArguments()
+    // TODO: options.short を考慮する.
 
-    for _, stringValue := range stringValues {
+    for _, stringValue := range options.values {
         infographicsText, success := InfographicsTextFromString(stringValue, " ")
         if !success {
             fmt.Println("ERROR: " + stringValue)
@@ -24,5 +18,31 @@ func main() {
         }
         fmt.Println(stringValue + " => " + infographicsText)
     }
+}
+
+type Options struct {
+    short bool
+    delimiter string
+    values []string
+}
+
+func parseArguments() Options {
+    var options Options
+
+    // TODO: Options.delimiter も解釈する.
+
+    for _, arg := range os.Args[1:] {
+        if arg == "-s" || arg == "--short" {
+            options.short = true
+            continue
+        }
+        if strings.HasPrefix(arg, "-") {
+            fmt.Println("WARN: Invalid option [" + arg + "]")
+            continue
+        }
+        options.values = append(options.values, arg)
+    }
+
+    return options
 }
 
