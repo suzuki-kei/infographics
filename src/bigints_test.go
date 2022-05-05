5af2145216dd2db573db890914eb55190e9a7087
SUZUKI Kei <todokimasen@gmail.com>
2022-05-05 17:18:58 +0900

    testCases から stringValues に変数名を変更した. 単なる値のスライスなので, testCases という名前は不適切と判断したため.

--- src/bigints_test.go
+++ src/bigints_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 func TestBigIntFromString(t *testing.T) {
-    testCases := []string{
+    stringValues := []string{
         "0",
         "1",
         "4294967296", // 2^32
         "18446744073709551616", // 2^64
         "1267650600228229401496703205376", // 2^100
     }
-    for _, stringValue := range testCases {
+    for _, stringValue := range stringValues {
         bigintValue, success := BigIntFromString(stringValue)
         assertEquals(t, true, success)
         assertEquals(t, stringValue, bigintValue.String())
