0c1aab5c10bcaac041bf8a41c9efed9d875b388f
SUZUKI Kei <todokimasen@gmail.com>
2022-05-05 17:51:58 +0900

    assert に成功した場合は関数の先頭で return するように変更した. エラーメッセージがソースコード上で読みやすいように変更した.

--- src/testing.go
+++ src/testing.go
@@ -2,18 +2,31 @@ package main
 
 import (
     "reflect"
+    "strings"
     "testing"
 )
 
 func assertEquals[T comparable](t *testing.T, expected T, actual T) {
-    if expected != actual {
-        t.Errorf("not equals.\n\texpected = %v\n\tactual = %v", expected, actual)
+    if expected == actual {
+        return
     }
+    format := strings.Join([]string{
+        "not equal.",
+        "expected = %v",
+        "actual= %v",
+    }, "\n")
+    t.Errorf(format, expected, actual)
 }
 
 func assertDeepEquals[T any](t *testing.T, expected T, actual T) {
-    if !reflect.DeepEqual(expected, actual) {
-        t.Errorf("not deep equals.\n\texpected = %v\n\tactual = %v", expected, actual)
+    if reflect.DeepEqual(expected, actual) {
+        return
     }
+    format := strings.Join([]string{
+        "not deep equal.",
+        "expected = %v",
+        "actual= %v",
+    }, "\n")
+    t.Errorf(format, expected, actual)
 }
 
